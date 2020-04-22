706a05c8029e188b452b58052cb3dd070204fcb5
zeley93 <becquart.quentin@gmail.com>
2020-04-22 17:33:25 +0200

    initialising variables + constructor

--- EthRoulette.sol
+++ EthRoulette.sol
@@ -1,5 +1,30 @@
 pragma solidity ^0.5.16;
 
 contract EthRoulette {
+
+  address payable owner;
+  uint8[] winMultiplicator;
+  uint8[] betTypeRange;
+  uint betPrice;
+  uint minBalance;
+  uint nextPlayableTimestamp;
+  mapping (address => uint256) winnings;
+  
+  struct Bet {
+    address player;
+    uint8 betType;
+    uint8 number; 
+  }
+  
+  Bet[] public bets;
+  
+  constructor() public payable {
+    owner = msg.sender;
+    nextPlayableTimestamp = now;
+    minBalance = 0;
+    winMultiplicator = [2,3,36];
+    betTypeRange = [1,1,36];
+    betPrice = 10000000000000000; /* 0.01 ether */
+  }
   
 }
